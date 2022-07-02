f92bc1e379d0ec5ab95c12e873653f124ce9d6a4
JhotaMS <Jhotams.developer@gmail.com>
2022-07-02 17:11:25 -0500

    Se unifican los cambios del archivo insuranceOS

--- src/index.ts
+++ src/index.ts
@@ -1,244 +1,91 @@
+import axios from "axios";
+import i = Sekure.Interface;
+import t = Sekure.Types;
 
-/* 
-import { GetProductById } from "./InsuranceOS/GetProductById"
-import { Quote } from "./InsuranceOS/Quote";
-import { ExecutableProduct } from "./Types/ExecutableProduct";
-import { GetProducts } from "./InsuranceOS/GetProducts"
-import { GetProductStage } from "./InsuranceOS/GetProductStage";
-import { GetEstimateBySessionId } from "./InsuranceOS/GetEstimateBySessionId";
-import { GetProductLotByName } from "./InsuranceOS/GetProductLotByName";
-import { Confirm } from "./InsuranceOS/Confirm";
-import { Emit } from "./InsuranceOS/Emit";
-import { QuoteLot } from "./InsuranceOS/QuoteLot";
-import { ExecutatbleProductLot } from "./Types/ExecutatbleProductLot";
-*/
+export default class InsuranceOS {
 
-import InsuranceOS from "./InsuranceOS";
+    private _url : string;
+    private _skrKey : string;
 
-
-
-
-
-
-
-//https://bobbyhadz.com/blog/typescript-http-request-axios
-//https://blog.logrocket.com/how-to-make-http-requests-like-a-pro-with-axios/
-
-
-/* GetProducts()
-  .then( resp => console.log( resp ) )
-  .catch( error => console.log( error ) )
-  .finally( () => console.log( `Fin GetProduct` ) ) */
-
-/* GetProductById(138)
-.then( resp => console.log( resp ) )
-.catch( error => console.log( error ) )
-.finally( () => console.log( `Fin GetProductById(138)` ) ) */
-
-/* GetProductStage('42bd068a-71df-4dcc-d4ec-08da2c5db10e')
-.then( resp => console.log( resp ) )
-.catch( error => console.log( error ) )
-.finally( () => console.log( "GetProductStage('42bd068a-71df-4dcc-d4ec-08da2c5db10e')" ) ) */
-
-/* GetEstimateBySessionId('42bd068a-71df-4dcc-d4ec-08da2c5db10e')
-.then( resp => console.log( resp ) )
-.catch( error => console.log( error ) )
-.finally( () => console.log( "GetEstimateBySessionId('42bd068a-71df-4dcc-d4ec-08da2c5db10e')" ) ) */
-
-/* GetProductLotByName('ground_test_pets')
-.then( resp => console.log( resp ) )
-.catch( error => console.log( error ) )
-.finally( () => console.log( "GetProductLotByName('ground_test_pets')" ) ) */
-
-
-/* const request : ExecutableProduct = {
-  ProductDetail : {
-      ProductId : 138,
-      ProductName : 'Asistencia Bolso Protegido',
-      PolicyTypeName : 'Asistencias',
-      InsuranceCompanyName : 'Andi Asistencia',
-      ProductCover : ''
-  },
-  PolicyHolder : {
-      firstName : 'Jhonatan',
-      secondName : 'Alejandro',
-      lastName : 'Muñoz',
-      secondLastName : 'Serna',
-      birthdate : new Date,
-      address : 'xxxxx',
-      identificationType : '1',
-      identificationNumber : '123456',
-      maritalStatus : '1',
-      email : 'jhonatan@sekure.com.co',
-      phoneNumber : '3112225588'
-  },
-  Parameters : []
-};
-
-console.log(request);
-console.log('Aquí empieza a cotizar');
-
-Quote(request)
-.then( resp => console.log( resp ) )
-.catch( error => console.log( error ) )
-.finally( () => console.log( "Quote(request)" ) ) */
-
-
-/* const sessionId = '993f1c03-a6e1-4271-7f98-08da5b789611';
-const request : ExecutableProduct = {
-  ProductDetail : {
-      ProductId : 31,
-      ProductName : 'Asistencia Telemedicina',
-      PolicyTypeName : 'Asistencias',
-      InsuranceCompanyName : 'Andi Asistencia',
-      ProductCover : ''
-  },
-  PolicyHolder : {
-      firstName : 'Jhonatan',
-      secondName : 'Alejandro',
-      lastName : 'Muñoz',
-      secondLastName : 'Serna',
-      birthdate : new Date,
-      address : 'xxxxx',
-      identificationType : '1',
-      identificationNumber : '123456',
-      maritalStatus : '1',
-      email : 'jhonatan@sekure.com.co',
-      phoneNumber : '3112225588'
-  },
-  Parameters : []
-};
-
-Confirm(request, sessionId)
-.then( resp => console.log( resp ) )
-.catch( error => console.log( error ) )
-.finally( () => console.log( "Confirm(request, sessionId)" ) ) */
-
-/* const sessionId = 'c6a51a72-6782-4719-7877-08da5b812142';
-const request : ExecutableProduct = {
-  ProductDetail : {
-      ProductId :             100,
-      ProductName :           'Exequias Individual',
-      PolicyTypeName :        'Plan Exequial',
-      InsuranceCompanyName :  'Prever',
-      ProductCover :          ''
-  },
-  PolicyHolder : {
-      firstName :             'Jhonatan',
-      secondName :            'Alejandro',
-      lastName :              'Muñoz',
-      secondLastName :        'Serna',
-      birthdate :             new Date,
-      address :               'xxxxx',
-      identificationType :    '1',
-      identificationNumber :  '123456',
-      maritalStatus :         '1',
-      email :                 'jhonatan@sekure.com.co',
-      phoneNumber :           '3112225588'
-  },
-  Parameters : [
+    constructor( url: string, skrKey: string ) 
     {
-      Name:                      'Lead Name',
-      InputParameterId:          330,
-      InputParameterType:        'Text',
-      inputParameterValue:       'Sekure',
-      InputParameterDescription: 'En este campo va el nombre del tomador de la póliza',
-      InputParameterRequired:    'true',
-      ShowApi:                   true,
-      InputParameterSchemaList:  []
-    },
-    {
-      Name:                      'Email',
-      InputParameterId:          331,
-      InputParameterType:        'Text',
-      inputParameterValue:       'contacto@sekure.com.co',
-      InputParameterDescription: 'Email del tomador',
-      InputParameterRequired:    'true',
-      ShowApi:                   true,
-      InputParameterSchemaList:  []
-    },
-    {
-      Name:                      'NameClient',
-      InputParameterId:          332,
-      InputParameterType:        'Text',
-      inputParameterValue:       'sekure',
-      InputParameterDescription: 'Nombre del cliente, usuario del API',
-      InputParameterRequired:    'true',
-      ShowApi:                   true,
-      InputParameterSchemaList:  []
+        this._url = url;
+        this._skrKey = skrKey; 
     }
-  ]
-};
 
-Emit(request, sessionId)
-.then( resp => console.log( resp ) )
-.catch( error => console.log( error ) )
-.finally( () => console.log( "Emit(request, sessionId)" ) ) */
+    public ConfigRequest(skrKey: string): any {
+        
+        if (process.env.NODE_ENV === 'development') {
+            console.log(skrKey);
+        }
+        return {
+            headers : {
+                Accept: "application/json",
+                "Content-Type": "application/json; charset=utf-8",
+                "Access-Control-Allow-Credentials": true,
+                "X-Requested-With": "XMLHttpRequest",
+                "skr-key": skrKey
+            }
+        }
+    }
 
-/* const request : ExecutatbleProductLot = {
-  BatchDetail : {
-    Name : 'Test_ground_Exequias_Individual',
-    PolicyTypeName : 'Asistencias'
-  },
-  Parameters : [
+    public GetProducts = async () : Promise<i.ProductDetail> => {
+
+        const { data } = await axios.get<i.ProductDetail>(`${ this._url }/Products/`, this.ConfigRequest( this._skrKey ) );
+        return data;
+    }
+
+    public GetProductById = async (id: number): Promise<i.Product> => 
     {
-      Name:                      'Fecha de nacimiento',
-      InputParameterId:          328,
-      InputParameterType:        'Text',
-      inputParameterValue:       '1991-10-10',
-      InputParameterDescription: 'Fecha de nacimiento del tomador, la edad máximo es de 70 años y el formato de fecha correcto es AÑO-MES-DÍA',
-      InputParameterRequired:    'true',
-      ShowApi:                   true,
-      InputParameterSchemaList:  []
-    },
+        const { data } = await axios.get<i.Product>(`${ this._url }/Products/${ id }`, this.ConfigRequest( this._skrKey ) );
+        return data;
+    }
+
+    public GetEstimateBySessionId = async ( sessionId : string ): Promise<i.Estimate> => 
     {
-      Name:                      'Periodo de vigencia',
-      InputParameterId:          329,
-      InputParameterType:        'Text',
-      inputParameterValue:       '1',
-      InputParameterDescription: 'Se debe de enviar los valores. 1 = Si es semestral, 2 = Si es anual',
-      InputParameterRequired:    'true',
-      ShowApi:                   true,
-      InputParameterSchemaList:  []
-    },
+        const { data } = await axios.get<i.Estimate>(`${ this._url }/Estimates/Session/${ sessionId }`, this.ConfigRequest( this._skrKey ) );
+        return data;
+    }
+
+    public GetQuoteBySessionId = async ( sessionId : string ) : Promise<i.Product> => 
     {
-      Name:                      'Ciudad',
-      InputParameterId:          351,
-      InputParameterType:        'Text',
-      inputParameterValue:       'Santa Fe de Antioquia',
-      InputParameterDescription: 'Ciudad del tomador',
-      InputParameterRequired:    'true',
-      ShowApi:                   true,
-      InputParameterSchemaList:  []
-    },
+        const { data } = await axios.get<i.Product>(`${ this._url  }/Quote/Session/${ sessionId }`, this.ConfigRequest( this._skrKey ) );
+        return data;
+    }
+
+    public GetProductStage = async ( sessionId : string ) : Promise<i.ProductStage> => 
     {
-      Name:                      'Dirección',
-      InputParameterId:          352,
-      InputParameterType:        'Text',
-      inputParameterValue:       'xxxxxx',
-      InputParameterDescription: 'Dirección del Tomador',
-      InputParameterRequired:    'true',
-      ShowApi:                   true,
-      InputParameterSchemaList:  []
-    },
-  ]
-};
+        const { data } = await axios.get<i.ProductStage>(`${ this._url }/Products/Stage/${ sessionId }`, this.ConfigRequest( this._skrKey ) );
+        return data;
+    }
 
-console.log(request);
-console.log('Aquí empieza a cotizar el lote');
+    public GetProductLotByName = async ( name : string ) : Promise<i.BatchDiscovery> => {
 
-QuoteLot(request)
-.then( resp => console.log( resp ) )
-.catch( error => console.log( error ) )
-.finally( () => console.log( "QuoteLot(request)" ) ) */
+        const { data } = await axios.get<i.BatchDiscovery>(`${ this._url  }/Products/Batch/${ name }`, this.ConfigRequest( this._skrKey ) );
+        return data;
+    }
 
-console.log('Inicio');
-let insuranceOS = new InsuranceOS('https://api.sekure.com.co/OSStage', 'ec151c0d3e6648f88b4a65fc3a0b1ab1');
-console.log('Entro al constructor');
+    public Quote = async (request : t.ExecutableProduct) : Promise<i.QuotedProduct> => {
 
-const getProductId = insuranceOS.GetProductById(138)
-  .then( resp => console.log(resp.Confirm[0].InputParameterSchemaList) )
-  .catch( error => console.log( error ) )
-  .finally( () => console.log( "GetProductById(138)" ) );
+        const { data } = await axios.post<i.QuotedProduct>(`${ this._url }/Products/Quote`, JSON.stringify( request ), this.ConfigRequest( this._skrKey ));
+        return data;
+    }
+      
+    public Confirm = async (request : t.ExecutableProduct, SessionId: string ) : Promise<( i.Policy | i.Exception )> => {
 
-  console.log(getProductId);
+        const { data } = await axios.post<( i.Policy | i.Exception )>(`${ this._url }/Products/Confirm/${ SessionId }`, JSON.stringify( request ), this.ConfigRequest( this._skrKey ));
+        return data;
+    }
+
+    public Emit = async (request : t.ExecutableProduct, SessionId: string ) : Promise<string> => {
+
+        const { data } = await axios.post<string>(`${ this._url }/Products/Emit/${ SessionId }`, JSON.stringify(request), this.ConfigRequest( this._skrKey ));
+        return data;
+    }
+
+    public QuoteLot = async ( request : t.ExecutatbleProductLot ) : Promise<( i.QuoteProductLot | i.Exception )> => {
+
+        const { data } = await axios.post<( i.QuoteProductLot | i.Exception )>(`${ this._url }/Products/Batch/Quote`, JSON.stringify(request), this.ConfigRequest( this._skrKey ));
+        return data;
+    }
+}
