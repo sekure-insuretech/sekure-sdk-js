c085aca97978859d253fd0b063d3fb7d48afbaa6
JhotaMS <Jhotams.developer@gmail.com>
2022-07-05 01:38:03 -0500

    correcciones

--- src/InsuranceOS.ts
+++ src/InsuranceOS.ts
@@ -7,85 +7,79 @@ export default class InsuranceOS {
     private _url : string;
     private _skrKey : string;
 
-    constructor( url: string, skrKey: string ) 
-    {
-        this._url = url;
-        this._skrKey = skrKey; 
-    }
+  constructor( url: string, skrKey: string ) 
+  {
+    this._url = url;
+    this._skrKey = skrKey; 
+  }
 
-    public ConfigRequest(skrKey: string): any {
-        
-        if (process.env.NODE_ENV === 'development') {
-            console.log(skrKey);
+  public ConfigRequest(skrKey: string): any {
+    
+    if (process.env.NODE_ENV === 'development') {
+        console.log(skrKey);
+    }
+    return {
+        headers : {
+            Accept: "application/json",
+            "Content-Type": "application/json; charset=utf-8",
+            "Access-Control-Allow-Credentials": true,
+            "X-Requested-With": "XMLHttpRequest",
+            "skr-key": skrKey
         }
-        return {
-            headers : {
-                Accept: "application/json",
-                "Content-Type": "application/json; charset=utf-8",
-                "Access-Control-Allow-Credentials": true,
-                "X-Requested-With": "XMLHttpRequest",
-                "skr-key": skrKey
-            }
-        }
     }
+  };
 
-    public GetProducts = async () : Promise<i.ProductDetail> => {
+  public GetProducts = async (): Promise<i.ProductDetail> => {
+    const { data } = await axios.get<i.ProductDetail>(`${ this._url }/Products/`, this.ConfigRequest( this._skrKey ) );
+    return data;
+  };
 
-        const { data } = await axios.get<i.ProductDetail>(`${ this._url }/Products/`, this.ConfigRequest( this._skrKey ) );
-        return data;
-    }
+  public GetProductById = async (id: number): Promise<i.Product> => 
+  {
+    const { data } = await axios.get<i.Product>(`${ this._url }/Products/${ id }`, this.ConfigRequest( this._skrKey ) );
+    return data;
+  };
 
-    public GetProductById = async (id: number): Promise<i.Product> => 
-    {
-        const { data } = await axios.get<i.Product>(`${ this._url }/Products/${ id }`, this.ConfigRequest( this._skrKey ) );
-        return data;
-    }
+  public GetEstimateBySessionId = async (sessionId : string): Promise<i.Estimate> => 
+  {
+    const { data } = await axios.get<i.Estimate>(`${ this._url }/Estimates/Session/${ sessionId }`, this.ConfigRequest( this._skrKey ) );
+    return data;
+  };
 
-    public GetEstimateBySessionId = async ( sessionId : string ): Promise<i.Estimate> => 
-    {
-        const { data } = await axios.get<i.Estimate>(`${ this._url }/Estimates/Session/${ sessionId }`, this.ConfigRequest( this._skrKey ) );
-        return data;
-    }
+  public GetQuoteBySessionId = async (sessionId : string) : Promise<i.Product> => 
+  {
+    const { data } = await axios.get<i.Product>(`${ this._url  }/Quote/Session/${ sessionId }`, this.ConfigRequest( this._skrKey ) );
+    return data;
+  };
 
-    public GetQuoteBySessionId = async ( sessionId : string ) : Promise<i.Product> => 
-    {
-        const { data } = await axios.get<i.Product>(`${ this._url  }/Quote/Session/${ sessionId }`, this.ConfigRequest( this._skrKey ) );
-        return data;
-    }
+  public GetProductStage = async (sessionId : string) : Promise<i.ProductStage> => 
+  {
+    const { data } = await axios.get<i.ProductStage>(`${ this._url }/Products/Stage/${ sessionId }`, this.ConfigRequest( this._skrKey ) );
+    return data;
+  };
 
-    public GetProductStage = async ( sessionId : string ) : Promise<i.ProductStage> => 
-    {
-        const { data } = await axios.get<i.ProductStage>(`${ this._url }/Products/Stage/${ sessionId }`, this.ConfigRequest( this._skrKey ) );
-        return data;
-    }
+  public GetProductLotByName = async (name : string) : Promise<i.BatchDiscovery> => {
+    const { data } = await axios.get<i.BatchDiscovery>(`${ this._url }/Products/Batch/${ name }`, this.ConfigRequest( this._skrKey ) );
+    return data;
+  };
 
-    public GetProductLotByName = async ( name : string ) : Promise<i.BatchDiscovery> => {
+  public Quote = async (request : t.ExecutableProduct) : Promise<i.QuotedProduct> => {
+    const { data } = await axios.post<i.QuotedProduct>(`${ this._url }/Products/Quote`, JSON.stringify( request ), this.ConfigRequest( this._skrKey ));
+    return data;
+  };
 
-        const { data } = await axios.get<i.BatchDiscovery>(`${ this._url  }/Products/Batch/${ name }`, this.ConfigRequest( this._skrKey ) );
-        return data;
-    }
+  public Confirm = async (request : t.ExecutableProduct, SessionId: string) : Promise<( i.Policy | i.Exception )> => {
+    const { data } = await axios.post<i.Policy>(`${ this._url }/Products/Confirm/${ SessionId }`, JSON.stringify( request ), this.ConfigRequest( this._skrKey ));
+    return data;
+  };
 
-    public Quote = async (request : t.ExecutableProduct) : Promise<i.QuotedProduct> => {
+  public Emit = async (request : t.ExecutableProduct, SessionId: string) : Promise<string> => {
+    const { data } = await axios.post<string>(`${ this._url }/Products/Emit/${ SessionId }`, JSON.stringify(request), this.ConfigRequest( this._skrKey ));
+    return data;
+  };
 
-        const { data } = await axios.post<i.QuotedProduct>(`${ this._url }/Products/Quote`, JSON.stringify( request ), this.ConfigRequest( this._skrKey ));
-        return data;
-    }
-      
-    public Confirm = async (request : t.ExecutableProduct, SessionId: string ) : Promise<( i.Policy | i.Exception )> => {
-
-        const { data } = await axios.post<( i.Policy | i.Exception )>(`${ this._url }/Products/Confirm/${ SessionId }`, JSON.stringify( request ), this.ConfigRequest( this._skrKey ));
-        return data;
-    }
-
-    public Emit = async (request : t.ExecutableProduct, SessionId: string ) : Promise<string> => {
-
-        const { data } = await axios.post<string>(`${ this._url }/Products/Emit/${ SessionId }`, JSON.stringify(request), this.ConfigRequest( this._skrKey ));
-        return data;
-    }
-
-    public QuoteLot = async ( request : t.ExecutatbleProductLot ) : Promise<( i.QuoteProductLot | i.Exception )> => {
-
-        const { data } = await axios.post<( i.QuoteProductLot | i.Exception )>(`${ this._url }/Products/Batch/Quote`, JSON.stringify(request), this.ConfigRequest( this._skrKey ));
-        return data;
-    }
+  public QuoteLot = async (request : t.ExecutatbleProductLot) : Promise<( i.QuoteProductLot | i.Exception )> => {
+    const { data } = await axios.post<i.QuoteProductLot>(`${ this._url }/Products/Batch/Quote`, JSON.stringify(request), this.ConfigRequest( this._skrKey ));
+    return data;
+  };
 }
--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,80 @@
-import InsuranceOS from "./InsuranceOS";
+import InsuranceOS from './InsuranceOS';
 import i = Sekure.Interface;
 import t = Sekure.Types;
 
 export default class Sekure {
+  private ios: InsuranceOS;
+  constructor(url: string, skrKey: string) {
+    this.ios = new InsuranceOS(url, skrKey);
+  }
 
-    private ios: InsuranceOS;
-    constructor( url: string, skrKey: string ) 
-    {
-        this.ios = new InsuranceOS(url, skrKey);
-    }
+  public GetProductById = async (id: number): Promise<i.Product> => {
+    return await this.ios
+      .GetProductById(id)
+      .then(res => res)
+      .catch(error => error);
+  };
 
-    public GetProductById = async (id: number) : Promise<i.Product> => {
-        return await this.ios.GetProductById(id).then((res) => res).catch((error) => error);
-    }
+  public GetProducts = async (): Promise<i.ProductDetail> => {
+    return await this.ios
+      .GetProducts()
+      .then(res => res)
+      .catch(error => error);
+  };
 
-    public GetProducts = async () : Promise<i.ProductDetail> => {
-      return await this.ios.GetProducts().then((res) => res).catch((error) => error);
-    }
+  public GetEstimateBySessionId = async (sessionId: string): Promise<i.Estimate> => {
+      return await this.ios
+        .GetEstimateBySessionId(sessionId)
+        .then(res => res)
+        .catch(error => error);
+  };
 
-    public GetEstimateBySessionId = async ( sessionId : string ): Promise<i.Estimate> => 
-    {
-        return await this.ios.GetEstimateBySessionId( sessionId ).then((res) => res).catch((error) => error);
-    }
+  public GetQuoteBySessionId = async (sessionId: string): Promise<i.Product> => {
+    return await this.ios
+      .GetQuoteBySessionId(sessionId)
+      .then(res => res)
+      .catch(error => error);
+  };
 
-    public GetQuoteBySessionId = async ( sessionId : string ) : Promise<i.Product> => 
-    {
-        return await this.ios.GetQuoteBySessionId( sessionId ).then((res) => res).catch((error) => error);
-    }
+  public GetProductStage = async (sessionId: string): Promise<i.ProductStage> => {
+    return await this.ios
+      .GetProductStage(sessionId)
+      .then(res => res)
+      .catch(error => error);
+  };
 
-    public GetProductStage = async ( sessionId : string ) : Promise<i.ProductStage> => 
-    {
-        return await this.ios.GetProductStage( sessionId ).then((res) => res).catch((error) => error);
-    }
+  public GetProductLotByName = async (name: string): Promise<i.BatchDiscovery> => {
+    return await this.ios
+      .GetProductLotByName(name)
+      .then(res => res)
+      .catch(error => error);
+  };
 
-    public GetProductLotByName = async ( name : string ) : Promise<i.BatchDiscovery> => {
+  public Quote = async (request: t.ExecutableProduct): Promise<i.QuotedProduct> => {
+    return await this.ios
+      .Quote(request)
+      .then(res => res)
+      .catch(error => error);
+  };
 
-        return await this.ios.GetProductLotByName( name ).then((res) => res).catch((error) => error);
-    }
+  public Confirm = async (request: t.ExecutableProduct, SessionId: string ): Promise<i.Policy> => {
+    return await this.ios
+      .Confirm(request, SessionId)
+      .then(res => res)
+      .catch(error => error);
+  };
 
-    public Quote = async (request : t.ExecutableProduct) : Promise<i.QuotedProduct> => {
+  public Emit = async (request: t.ExecutableProduct, SessionId: string): Promise<string> => {
+    return await this.ios
+      .Emit(request, SessionId)
+      .then(res => res)
+      .catch(error => error);
+  };
 
-        return await this.ios.Quote( request).then((res) => res).catch((error) => error);
-    }
-
-    public Confirm = async ( request : t.ExecutableProduct, SessionId: string ) : Promise<( i.Policy | i.Exception )> => {
-
-        return await this.ios.Confirm( request, SessionId ).then((res) => res).catch((error) => error);
-    }
-
-    public Emit = async ( request : t.ExecutableProduct, SessionId: string ) : Promise<string> => {
-
-        return await this.ios.Emit( request, SessionId ).then((res) => res).catch((error) => error);
-    }
-
-    public QuoteLot = async ( request : t.ExecutatbleProductLot ) : Promise<( i.QuoteProductLot | i.Exception )> => {
-
-        return await this.ios.QuoteLot( request ).then((res) => res).catch((error) => error);
-    }
+  public QuoteLot = async (request: t.ExecutatbleProductLot): Promise<(i.QuoteProductLot)> => {
+    return await this.ios
+      .QuoteLot(request)
+      .then(res => res)
+      .catch(error => error);
+  };
 }
